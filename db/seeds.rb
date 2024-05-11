f3d9dbb877d979f4389d5ef271faba77f4e07a7a
Martin Ferrari <dmferrari@gmail.com>
2024-05-11 18:25:51 -0300

    Seed database data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+DEFAULT_PASSWORD = 'password'
+
+def create_user(email:, roles:)
+  created_user = User.find_or_create_by!(email:) do |user|
+    user.full_name = Faker::Name.name
+    user.password = DEFAULT_PASSWORD
+    user.password_confirmation = DEFAULT_PASSWORD
+  end
+  roles.each { |role| created_user.add_role(role) }
+end
+
+# Create the admin user
+create_user(email: 'admin@clinic.com', roles: %w[admin])
+
+# Create some doctors
+10.times do
+  create_user(email: Faker::Internet.email, roles: %w[doctor patient])
+end
+
+# Create some patients
+10.times do
+  create_user(email: Faker::Internet.email, roles: %w[patient])
+end
