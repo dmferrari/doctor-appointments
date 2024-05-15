88f786f12381c22c81079d24c09856ec1bbddced
Martin Ferrari <dmferrari@gmail.com>
2024-05-14 22:19:28 -0300

    Add DoctorProfile model

---
+++ app/models/doctor_profile.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class DoctorProfile < ApplicationRecord
+  belongs_to :doctor, class_name: 'User', inverse_of: :doctor_profile
+
+  validates :session_length, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :specialty, presence: true
+end
---
+++ db/migrate/20240514101527_create_doctor_profiles.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CreateDoctorProfiles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :doctor_profiles do |t|
+      t.references :doctor, null: false, foreign_key: { to_table: :users }
+      t.integer :session_length, null: false
+      t.string :specialty
+
+      t.timestamps
+    end
+
+    # add_index :doctor_profiles, :doctor_id, unique: true
+    add_index :doctor_profiles, :specialty
+  end
+end
