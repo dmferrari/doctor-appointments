8807e0242d9f807a8139e16b1dc376f8963f3d4d
Martin Ferrari <dmferrari@gmail.com>
2024-05-14 22:15:51 -0300

    Add scopes for doctors and patients

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
+  scope :doctors, -> { with_role(:doctor) }
+  scope :patients, -> { with_role(:patient) }
+
   has_many :working_hours, inverse_of: :doctor, dependent: :destroy
 
   has_many :doctor_appointments, class_name: 'Appointment', foreign_key: 'doctor_id', inverse_of: :doctor,
