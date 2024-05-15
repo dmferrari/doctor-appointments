f97b57c14e4d38612e408d9f2637846cfbe2173f
Martin Ferrari <dmferrari@gmail.com>
2024-05-14 22:14:13 -0300

    Relocated rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  rolify
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
-  rolify
 end
