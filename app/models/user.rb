69001850c7ae5a8f2e0bcc23a4c3f01c2af946c9
Martin Ferrari <dmferrari@gmail.com>
2024-05-14 22:15:35 -0300

    Define associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
+  has_many :working_hours, inverse_of: :doctor, dependent: :destroy
+
+  has_many :doctor_appointments, class_name: 'Appointment', foreign_key: 'doctor_id', inverse_of: :doctor,
+                                 dependent: :destroy
+  has_many :patient_appointments, class_name: 'Appointment', foreign_key: 'patient_id', inverse_of: :patient,
+                                  dependent: :destroy
+
 end
