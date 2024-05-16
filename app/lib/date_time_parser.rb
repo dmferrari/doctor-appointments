7f03500711dff17a6390ed2577f6094a968650ce
Martin Ferrari <dmferrari@gmail.com>
2024-05-16 19:33:48 -0300

    Refactor date and time parsing logic

---
+++ app/lib/date_time_parser.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module DateTimeParser
+  def string_to_time(time_str)
+    string_to_time!(time_str)
+  rescue ArgumentError
+    nil
+  end
+
+  def string_to_time!(time_str)
+    Time.strptime(time_str, I18n.t('time_format'))
+  rescue ArgumentError
+    raise ArgumentError, I18n.t('errors.messages.invalid_time_format')
+  end
+
+  def string_to_date(date_str)
+    string_to_date!(date_str)
+  rescue ArgumentError
+    nil
+  end
+
+  def string_to_date!(date_str)
+    Date.parse(date_str)
+  rescue ArgumentError
+    raise ArgumentError, I18n.t('errors.messages.invalid_date_format')
+  end
+end
