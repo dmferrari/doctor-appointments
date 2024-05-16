c6096f3d6f0cf376bbbd40983c721eb965b0bc9e
Martin Ferrari <dmferrari@gmail.com>
2024-05-16 19:41:55 -0300

    Refactor service

--- app/services/doctor_availability_service.rb
+++ app/services/doctor_availability_service.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class DoctorAvailabilityService
-  include TimeParser
+  include DateTimeParser
+
+  class DateRangeNotAllowedError < StandardError; end
 
   attr_reader :doctor, :date
 
   def initialize(doctor:, date: nil)
     @doctor = doctor
-    @date = date
+    @date = resolve_date(date)
   end
 
   def working_hours
@@ -15,93 +17,79 @@ class DoctorAvailabilityService
   end
 
   def availability
-    @availability ||= calculate_availability(@date)
+    @availability ||= calculate_availability
   end
 
   def available_at?(start_time:)
-    end_time = string_to_time(start_time) + doctor.doctor_profile.session_length.minutes
-    availability.any? do |slot|
-      string_to_time(slot[:start_time]) <= string_to_time(start_time) &&
-        end_time <= string_to_time(slot[:end_time])
-    end
+    ensure_date_is_not_a_range
+    end_time = calculate_end_time(start_time)
+    availability.any? { |slot| within_time_range?(slot[:start_time], slot[:end_time], start_time, end_time) }
+  end
+
+  def appointment_within_working_hours?(start_time:, end_time:)
+    ensure_date_is_not_a_range
+    working_hour = fetch_working_hours(@date).first
+    within_time_range?(working_hour.start_time, working_hour.end_time, start_time, end_time)
   end
 
   private
 
-  # Normalizes the given date by returning it if not nil, or a range from today
-  # to one week from now if nil.
-  #
-  # @param date [Date, nil] The date to be normalized.
-  # @return [Range] The normalized date range.
-  def normalize_date(date)
+  def ensure_date_is_not_a_range
+    raise DateRangeNotAllowedError, I18n.t('errors.messages.date_range_not_allowed') if @date.is_a?(Range)
+  end
+
+  def resolve_date(date)
     date || (Time.zone.today..1.week.from_now)
   end
 
   def fetch_working_hours(date)
-    normalized_date = normalize_date(date)
-    WorkingHour.where(doctor_id: @doctor.id, working_date: normalized_date)
+    WorkingHour.where(doctor: @doctor).where(working_date: date)
   end
 
-  def calculate_availability(date)
-    normalized_date = normalize_date(date)
-    if normalized_date.is_a?(Range)
-      normalized_date.flat_map { |d| availability_on_date(d) }
-    else
-      availability_on_date(normalized_date)
-    end
+  def calculate_availability
+    dates = @date.is_a?(Range) ? @date : [@date]
+    dates.flat_map { |date| daily_availability(date) }
   end
 
-  def availability_on_date(date)
-    working_hours = fetch_working_hours(date).first
-    return [] unless working_hours
+  def daily_availability(date)
+    day_working_hours = fetch_working_hours(date).first
+    return [] unless day_working_hours
 
-    appointments = Appointment.where(doctor_id: @doctor.id, appointment_date: date).order(:start_time)
-    calculate_free_intervals(date, working_hours, appointments)
+    appointments = @doctor.doctor_appointments.where(appointment_date: date).order(:start_time)
+    calculate_free_intervals(date, day_working_hours, appointments)
   end
 
-  # Calculates the free intervals within a given date, working hours, and
-  # appointments.
-  #
-  # @param date [Date] The date for which to calculate the free intervals.
-  # @param working_hours [WorkingHours] The working hours during which the
-  #   intervals should be calculated.
-  # @param appointments [Array<Appointment>] The list of appointments to
-  #   consider when calculating the intervals.
-  # @return [Array<Interval>] An array of intervals representing the free time
-  #   slots.
-  def calculate_free_intervals(date, working_hours, appointments)
+  def calculate_free_intervals(date, day_working_hours, appointments)
     free_intervals = []
-    current_time = working_hours.start_time
+    current_time = day_working_hours.start_time
 
-    # Add the free interval before and between each appointment
-    appointments.each do |appointment|
-      if current_time < appointment.start_time
-        interval = build_interval(date:, start_time: current_time, end_time: appointment.start_time)
-        free_intervals << interval if interval_fits_session_length?(interval)
-      end
-      current_time = appointment.end_time if current_time < appointment.end_time
+    appointments.each do |appt|
+      add_free_interval_if_valid(free_intervals, date, current_time, appt.start_time) if current_time < appt.start_time
+      current_time = [current_time, appt.end_time].max
     end
 
-    # Add the last free interval if available
-    if current_time < working_hours.end_time
-      interval = build_interval(date:, start_time: current_time, end_time: working_hours.end_time)
-      free_intervals << interval if interval_fits_session_length?(interval)
+    if current_time < day_working_hours.end_time
+      add_free_interval_if_valid(free_intervals, date, current_time, day_working_hours.end_time)
     end
 
     free_intervals
   end
 
-  def build_interval(date:, start_time:, end_time:)
-    { date:, start_time:, end_time: }
+  def add_free_interval_if_valid(free_intervals, date, start_time, end_time)
+    free_interval = { date:, start_time:, end_time: }
+    free_intervals << free_interval if interval_meets_minimum_session_length?(free_interval)
   end
 
-  # Checks if the interval fits the session length of the doctor.
-  #
-  # @param interval [Hash] The interval to check.
-  # @return [Boolean] True if the interval fits the session length, false
-  #   otherwise.
-  def interval_fits_session_length?(interval)
+  def interval_meets_minimum_session_length?(interval)
     interval_in_minutes = ((string_to_time(interval[:end_time]) - string_to_time(interval[:start_time])) / 60).minutes
     interval_in_minutes >= doctor.doctor_profile.session_length.minutes
+  end
+
+  def calculate_end_time(start_time)
+    string_to_time(start_time) + doctor.doctor_profile.session_length.minutes
+  end
+
+  def within_time_range?(range_start, range_end, start_time, end_time)
+    range_start <= start_time && end_time <= range_end
   end
 end
