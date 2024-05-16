9a8c6c5c177d2a571f831ba11066de4f525ba8e5
Martin Ferrari <dmferrari@gmail.com>
2024-05-16 07:57:30 -0300

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,7 @@ class User < ApplicationRecord
 
   include Doctorable
 
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :full_name, presence: true, length: { minimum: 3 }
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
