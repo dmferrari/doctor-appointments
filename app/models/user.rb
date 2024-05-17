078fe18f1c94eb54916582a80caf8cab95cb849e
Martin Ferrari <dmferrari@gmail.com>
2024-05-17 11:35:54 -0300

    Check if an user has all roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :full_name, presence: true, length: { minimum: 3 }
   validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+
+  def has_all_roles?(*roles) # rubocop:disable Naming/PredicateName
+    roles.all? { |role| has_role?(role) }
+  end
 end
