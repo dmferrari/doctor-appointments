b1f15fe9039dbea1f691581eb49ee379755a7525
Martin Ferrari <dmferrari@gmail.com>
2024-05-17 11:39:36 -0300

    Factory to create patients and doctors

---
+++ spec/factories/user.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    full_name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { 'password123' }
+    password_confirmation { 'password123' }
+
+    trait :doctor do
+      after(:create) { |user| user.add_role(:doctor) }
+    end
+
+    trait :patient do
+      after(:create) { |user| user.add_role(:patient) }
+    end
+
+    trait :doctor_and_patient do
+      after(:create) do |user|
+        user.add_role(:doctor)
+        user.add_role(:patient)
+      end
+    end
+  end
+end
