4991eaa7dc737301f7c000170cc8b28e92982142
Martin Ferrari <dmferrari@gmail.com>
2024-05-19 19:58:16 -0300

    Move constants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,10 @@
 # frozen_string_literal: true
 
+include Constants
+
 # Of course, this is an hardcoded password just for the sake of the example
 # This will allow you to login as any user just knowing their email and this
 # password
-DEFAULT_PASSWORD = 'password'
-
-SESSION_START_TIMES = %w[08:00 09:00 10:00].freeze
-SESSION_END_TIMES = %w[16:00 17:00 18:00 19:00 20:00].freeze
-SESSION_LENGTHS = [15, 30, 45, 60].freeze
-
-APPOINTMENT_START_TIMES = %w[08:00 09:00 10:00 11:00 12:00 13:00 14:00 15:00].freeze
-
-DOCTOR_SPECIALTIES = %w[
-  Cardiology
-  Dermatology
-  Endocrinology
-  Gastroenterology
-  Neurology
-  Oncology
-  Orthopedics
-  Pediatrics
-  Psychiatry
-  Radiology
-].freeze
 
 def create_user(email:, roles:)
   created_user = User.find_or_create_by!(email:) do |user|
---
+++ lib/constants.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Constants
+  DEFAULT_PASSWORD = 'password'
+
+  SESSION_START_TIMES = %w[08:00 09:00 10:00].freeze
+  SESSION_END_TIMES = %w[16:00 17:00 18:00 19:00 20:00].freeze
+  SESSION_LENGTHS = [15, 30, 45, 60].freeze
+
+  APPOINTMENT_START_TIMES = %w[08:00 09:00 10:00 11:00 12:00 13:00 14:00 15:00].freeze
+
+  DOCTOR_SPECIALTIES = %w[
+    Cardiology
+
+    Dermatology
+    Endocrinology
+    Gastroenterology
+    Neurology
+    Oncology
+    Orthopedics
+    Pediatrics
+    Psychiatry
+    Radiology
+  ].freeze
+end
