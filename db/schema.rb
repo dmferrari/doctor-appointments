db9e3aa9e36d160c2fd3dc8b12a28acf5ab0e39b
Martin Ferrari <dmferrari@gmail.com>
2024-05-19 20:02:45 -0300

    Updated schema based on latest migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_101527) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_19_222643) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_101527) do
     t.string "end_time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
     t.index ["appointment_date"], name: "index_appointments_on_appointment_date"
+    t.index ["deleted_at"], name: "index_appointments_on_deleted_at"
     t.index ["doctor_id", "patient_id", "appointment_date"], name: "idx_on_doctor_id_patient_id_appointment_date_52b0e581f8", unique: true
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
@@ -65,6 +67,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_101527) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "locale", default: "en"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
