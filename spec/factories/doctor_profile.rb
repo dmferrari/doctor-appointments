00898b497b36ed10770254c414948018bf09def6
Martin Ferrari <dmferrari@gmail.com>
2024-05-19 22:50:57 -0300

    Use constants

--- spec/factories/doctor_profile.rb
+++ spec/factories/doctor_profile.rb
@@ -1,22 +1,11 @@
 # frozen_string_literal: true
 
-DOCTOR_SPECIALTIES = %w[
-  Cardiology
-  Dermatology
-  Endocrinology
-  Gastroenterology
-  Neurology
-  Oncology
-  Orthopedics
-  Pediatrics
-  Psychiatry
-  Radiology
-].freeze
+include Constants
 
 FactoryBot.define do
   factory :doctor_profile do
     association :doctor, factory: :user, strategy: :build
     specialty { DOCTOR_SPECIALTIES.sample }
-    session_length { %w[15 30 45 60].sample }
+    session_length { SESSION_LENGTHS.sample }
   end
 end
--- spec/factories/working_hour.rb
+++ spec/factories/working_hour.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-SESSION_START_TIMES = %w[08:00 09:00 10:00].freeze
-SESSION_END_TIMES = %w[16:00 17:00 18:00 19:00 20:00].freeze
+include Constants
 
 FactoryBot.define do
   factory :working_hour do
