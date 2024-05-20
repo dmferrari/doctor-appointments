3e78ace0d9ea8fa128d8e629d59c59d1151cabf7
Martin Ferrari <dmferrari@gmail.com>
2024-05-19 22:49:37 -0300

    Removed unneeded validations

--- app/services/doctor_availability_service.rb
+++ app/services/doctor_availability_service.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 class DoctorAvailabilityService
-  include ActiveModel::Validations
   include DateTimeParser
 
   class DateRangeNotAllowedError < StandardError; end
-
-  validates :date, presence: true
-  validates :doctor, presence: true
 
   attr_reader :doctor, :date
 
