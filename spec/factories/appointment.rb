354a771b66b0d52cfaf0dad85c8d8ce30221b4e9
Martin Ferrari <dmferrari@gmail.com>
2024-05-19 22:51:08 -0300

    Updated start time to avoid issues

--- spec/factories/appointment.rb
+++ spec/factories/appointment.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+include Constants
+
 FactoryBot.define do
   factory :appointment do
     doctor { create(:user, :doctor) }
     patient { create(:user, :patient) }
     appointment_date { Faker::Date.forward(days: 5) }
-    start_time { '10:00' }
+    start_time { '14:00' }
   end
 end
