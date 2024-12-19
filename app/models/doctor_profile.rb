dc07a8c4c61c84588b6abcebf7381c5fc4367fbd
Martin Ferrari <dmferrari@gmail.com>
2024-12-19 19:35:18 -0300

    Add doctor card

--- app/models/doctor_profile.rb
+++ app/models/doctor_profile.rb
@@ -5,4 +5,8 @@ class DoctorProfile < ApplicationRecord
 
   validates :session_length, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :specialty, presence: true
+
+  def card_info
+    "#{doctor.full_name} - #{specialty}"
+  end
 end
--- spec/models/doctor_profile_spec.rb
+++ spec/models/doctor_profile_spec.rb
@@ -4,5 +4,12 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe DoctorProfile, type: :model do
-  skip 'Awaiting implementation'
+  describe '#card_info' do
+    it 'returns the doctor name and specialty' do
+      doctor = create(:user, full_name: 'Dr. House')
+      doctor_profile = create(:doctor_profile, doctor:, specialty: 'Diagnostic Medicine')
+
+      expect(doctor_profile.card_info).to eq('Dr. House - Diagnostic Medicine')
+    end
+  end
 end
